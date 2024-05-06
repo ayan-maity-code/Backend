861f3cc749bed8275769a81bb4d3926a4c288691
Ayan2002Maity <ayanmaity943@gmail.com>
2024-05-06 16:05:50 +0530

    data modeling and middlewares

--- Mega Project/src/index.js
+++ Mega Project/src/index.js
@@ -1,12 +1,20 @@
-
 import dotenv from 'dotenv'
 import connectDb from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
   path: './env'
 })
 
 connectDb()
+.then(() =>{
+  app.listen(process.env.PORT || 8000 , () =>{
+    console.log(`Server is running on port : ${process.env.PORT}`)
+  })
+})
+.catch((err) =>{
+    console.log('MONGO db connection failed :' , err)
+})
 
 
 
